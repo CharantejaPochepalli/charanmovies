abe4cc0ddeac34a6f9614cdf39d6e0ed8a6f31a7
Charan teja <charantejapochepalli@gmail.com>
2023-09-25 12:07:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";// we are importing BrowserRouter as Router,Routes,Route from react-router-dom that we imported
+import Header from './components/header/header.js';
+import Home from './pages/home/home';
+import MovieList from './components/header/movielist/movieList';
+import Movie  from './pages/home/movieDetails/movie';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <Header />
+            <Routes>
+                <Route index element={<Home />}></Route>
+                <Route path="movie/:id" element={<Movie />}></Route>
+                <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
+// function App() {
+//    return (
+//     <div className="App">
+//        <Router>
+//         <Header/>
+//              <Routes>
+//                     <Route index element={<Home/>}></Route>
+//                     <Route  path="movie/:id" element={<Movie/>}></Route>
+//                     <Route path="movie/:type" element={<MovieList/>} ></Route>
+//                     <Route path="/*" element={<h1>Error page</h1>}></Route>
+//                </Routes>
+//        </Router> 
+//     </div>
+//   );
+// }
